b5eede67a82a70d3d6b9774e7897eba02075c7f6
googlefan256 <googlefan@neody.dev>
2025-05-08 14:24:09 +0900

    fix

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,8 +1,8 @@
 rm -rf ./store/voices*
-huggingface-cli download onnx-community/Kokoro-82M-v1.0-ONNX --include "voices/jf_*.bin" --local-dir ./store/voices_tmp
+uv run huggingface-cli download onnx-community/Kokoro-82M-v1.0-ONNX --include "voices/jf_*.bin" --local-dir ./store/voices_tmp
 mv ./store/voices_tmp/voices ./store/voices
 rm -rf ./store/voices_tmp
-huggingface-cli download onnx-community/Kokoro-82M-v1.0-ONNX --include "onnx/model.onnx" --local-dir ./store/onnx_tmp
+uv run huggingface-cli download onnx-community/Kokoro-82M-v1.0-ONNX --include "onnx/model.onnx" --local-dir ./store/onnx_tmp
 mv ./store/onnx_tmp/onnx/model.onnx ./store/model.onnx
 rm -rf ./store/onnx_tmp
-python -m unidic download
+uv run python -m unidic download
